4f53e81eb8357e4bd9a1f5b04fff1e5f75669adf
eurodrigobispo <innapse.agencia@gmail.com>
2026-06-23 13:38:47 -0300

    feat(editor): editor vira o entry do app

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import ProposalDocument from "./render/ProposalDocument.jsx";
-import { baseProposal } from "./data/baseProposal.js";
+import EditorApp from "./editor/EditorApp.jsx";
 
-createRoot(document.getElementById("root")).render(<ProposalDocument data={baseProposal} />);
+createRoot(document.getElementById("root")).render(<EditorApp />);
